234a9de0a2adfb0764255536cba436e8d5592a0a
MISTICMAN23 <132823841+723mist@users.noreply.github.com>
2025-10-12 10:09:55 +0300

    Delete core_old directory

--- core_old/graphics/objects.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-void drawRect(float pos_x = 0, float pos_y = 0, float scale_x = 100, float scale_y = 100, float red = 1.0f, float green = 1.0f, float blue = 1.0f);
-void drawCircle(float c_pos_x = 0, float c_pos_y = 0, float radius = 10, int num_segments = 10, float red = 1.0f, float green = 1.0f, float blue = 1.0f);
