0dea7c8f9a76e064e275fa9df061d9c78cb6cba0
MISTICMAN23 <132823841+723mist@users.noreply.github.com>
2025-10-22 21:39:21 +0300

    Add texture and EBO

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,23 +1,31 @@
 extern crate gl;
 extern crate glfw;
 extern crate gl_loader;
+extern crate image;
 
 use gl::types::*;
 use glfw::{Context};
 use std::mem::size_of_val;
 use std::ffi::CString;
+use std::path::Path;
+
+#[allow(unused_imports)]
+use image::GenericImageView;
 
 const VERTEX_SHADER_SOURCE: &str = r#"
     #version 330 core
-    layout (location = 0) in vec3 aPos;    // позиция вершины
-    layout (location = 1) in vec3 aColor;  // цвет вершины
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+    layout (location = 2) in vec2 aTexCoord;
 
-    out vec3 ourColor; // передаём во фрагментный шейдер
+    out vec3 ourColor;
+    out vec2 TexCoord;
 
     void main()
     {
         gl_Position = vec4(aPos, 1.0);
-        ourColor = aColor; // просто передаём цвет дальше
+        ourColor = aColor;
+        TexCoord = aTexCoord;
     }
 "#;
 
@@ -25,11 +33,14 @@ const FRAGMENT_SHADER_SOURCE: &str = r#"
     #version 330 core
     out vec4 FragColor;
 
-    in vec3 ourColor; // принимаем цвет из vertex shader
+    in vec3 ourColor;
+    in vec2 TexCoord;
+
+    uniform sampler2D ourTexture;
 
     void main()
     {
-        FragColor = vec4(ourColor, 1.0); // рисуем этим цветом
+        FragColor = texture(ourTexture, TexCoord);
     }
 "#;
 
@@ -49,17 +60,46 @@ pub fn window(title: &str, width: u32, height: u32) {
     gl::load_with(|symbol| gl_loader::get_proc_address(symbol) as *const _);
 
     let vertices: Vec<f32> = vec![
-        0.0,  0.5, 0.0,  1.0, 0.0, 0.0,
-        -0.5, -0.5, 0.0,  0.0, 1.0, 0.0,
-        0.5, -0.5, 0.0,  0.0, 0.0, 1.0,
+        0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0,   // top right
+        0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 1.0,   // bottom right
+        -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 1.0,   // bottom left
+        -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 0.0
     ];
+
+    let _tex_coords: Vec<f32> = vec![
+        0.0, 0.0,
+        1.0, 0.0,
+        0.5, 1.0
+    ];
+
+    let border_color: Vec<f32> = vec!{ 1.0, 1.0, 0.0, 1.0 };
 
     let mut vbo: u32 = 0;
     let mut vao: u32 = 0;
+    let mut ebo: u32 = 0;
+
+    let indices: Vec<u32> = vec!{
+        0, 1, 3,
+        1, 2, 3
+    };
 
     let vertex_shader: u32;
     let fragment_shader: u32;
     let shader_program: u32;
+
+    let mut texture: u32 = 0;
+
+    //Texture path
+    let texture_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("r/w.jpg");
+
+    //Image + error
+    let img = image::open(texture_path).unwrap_or_else(|e| panic!("Failed to load texture: {}", e));
+
+    //Image type RGBA
+    let img_rgba = img.to_rgba8();
+
+    //Image dimensions
+    let (width, height) = img_rgba.dimensions();
 
     //SHADER(VBO)
     unsafe {
@@ -68,6 +108,7 @@ pub fn window(title: &str, width: u32, height: u32) {
         fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
         shader_program = gl::CreateProgram();
 
+        //Vertex and fragment sources
         let vertex_source = CString::new(VERTEX_SHADER_SOURCE).unwrap();
         let fragment_source = CString::new(FRAGMENT_SHADER_SOURCE).unwrap();
 
@@ -92,21 +133,6 @@ pub fn window(title: &str, width: u32, height: u32) {
 
         //Use program shader
         gl::UseProgram(shader_program);
-
-        //Gen buffers
-        //gl::GenBuffers(1, &mut vbo);
-
-        //Bind buffer
-        //gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-
-        //Bufer data
-        //gl::BufferData(gl::ARRAY_BUFFER, size_of_val(vertices.as_slice()) as isize, vertices.as_ptr() as *const GLvoid, gl::STATIC_DRAW);
-
-        //Vertex attrib pointer
-        //gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, (6 * std::mem::size_of::<f32>()) as i32, std::ptr::null());
-
-        //Enable vertex attrib array
-        //gl::EnableVertexAttribArray(1);
     }
 
     //SHADER(VAO)
@@ -125,13 +151,52 @@ pub fn window(title: &str, width: u32, height: u32) {
         //Buffer data
         gl::BufferData(gl::ARRAY_BUFFER, size_of_val(vertices.as_slice()) as isize, vertices.as_ptr() as *const GLvoid, gl::STATIC_DRAW);
 
-        // Позиция
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 6 * 4, std::ptr::null());
-        gl::EnableVertexAttribArray(0);
+        //Vertex attrib pointer
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 8 * 4, std::ptr::null());
+        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, 8 * 4, (3 * 4) as *const _);
+        gl::VertexAttribPointer(2, 2, gl::FLOAT, gl::FALSE, 8 * 4, (6 * 4) as *const _);
 
-        // Цвет
-        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, 6 * 4, (3 * 4) as *const _);
+        //Enable vertex attrib array(color)
+        gl::EnableVertexAttribArray(0);
         gl::EnableVertexAttribArray(1);
+        gl::EnableVertexAttribArray(2);
+    }
+
+    //EBO
+    unsafe {
+        //Gen buffer(for EBO)
+        gl::GenBuffers(1, &mut ebo);
+
+        //Bind buffer
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+
+        //Buffer data
+        gl::BufferData(gl::ELEMENT_ARRAY_BUFFER, size_of_val(indices.as_slice()) as isize, indices.as_ptr() as *const GLvoid, gl::STATIC_DRAW);
+    }
+
+    //Textures
+    unsafe {
+        //Generate texture
+        gl::GenTextures(1, &mut texture);
+
+        //Bind texture
+        gl::BindTexture(gl::TEXTURE_2D, texture);
+
+        //Texture parameteri
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::MIRRORED_REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::MIRRORED_REPEAT as i32);
+
+        //Texture parameterfv
+        gl::TexParameterfv(gl::TEXTURE_2D, gl::TEXTURE_BORDER_COLOR, border_color.as_ptr());
+
+        //Texture parameteri
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+
+        //Texture image 2D
+        gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGBA as i32, width as i32, height as i32, 0, gl::RGBA, gl::UNSIGNED_BYTE, img_rgba.as_ptr() as *const GLvoid);
+
+        //Generate mipmap
+        gl::GenerateMipmap(gl::TEXTURE_2D);
     }
 
     //Main loop
@@ -141,9 +206,11 @@ pub fn window(title: &str, width: u32, height: u32) {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
+            gl::BindTexture(gl::TEXTURE_2D, texture);
             gl::BindVertexArray(vao);
 
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
+            //gl::DrawArrays(gl::TRIANGLES, 0, 6); //Old
         }
         glfw.poll_events();
     }
